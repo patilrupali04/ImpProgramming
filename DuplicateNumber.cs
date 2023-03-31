c4070581ebd8403d9f2f661367772ca5914a620c
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-03-31 06:59:58 +0530

    find duplicate number using array

---
+++ DuplicateNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array3
+{
+    class DuplicateNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = {20,45,10,33,5,40,33,9 };
+            for(int i = 0; i <= arr1.Length - 1; i++)
+            {
+                for(int j=i+1; j <= arr1.Length - 1; j++)
+                {
+                    if (arr1[i] == arr1[j])
+                    {
+                        Console.WriteLine("Duplicate:: {0}", arr1[i]);
+                    }
+                }
+            }
+        }
+    }
+}
