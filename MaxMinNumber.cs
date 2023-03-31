6666fdcc0b72ff180d9c32cfc833a7cdb84c88cf
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-03-31 06:58:47 +0530

    Array

---
+++ MaxMinNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMinNumber
+{
+    class MaxMinNumber
+    {
+        static void Main(string[] args)
+        {
+            //Find maximum and minimum number 
+            int[] arr1 = { 10,15,45,55,65,85,99,56,12,1,78,88};
+            int max = arr1[0];
+            int min = arr1[0];
+            for(int i = 0; i <= arr1.Length - 1; i++)
+            {
+                if (arr1[i] > max)
+                {
+                    max = arr1[i];
+                }
+                if (arr1[i] < min)
+                {
+                    min = arr1[i];
+                }
+            }
+            Console.WriteLine("Max: {0}", max);
+            Console.WriteLine("Min: {0}", min);
+        }
+    }
+
+    //output: min-1, max-99
+}
