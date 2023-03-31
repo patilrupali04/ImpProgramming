1b7193c11186ef9de6d4c83b9ea39b3a42cba4f1
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-03-31 07:01:05 +0530

    Add files via upload

---
+++ PalindromeString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string1
+{
+    class PalindromeString
+    {
+        static void Main(string[] args)
+        {
+            string str = "level";
+            Console.WriteLine("input string: {0}", str);
+            char[] arr1 = str.ToCharArray();
+            string rev = "";
+            for (int i=arr1.Length-1;i>=0;i--)
+            {
+                rev = rev + arr1[i];
+            }
+            Console.WriteLine("Reversed: {0}", rev);
+
+            if (str.Equals(rev))
+            {
+                Console.WriteLine(str + " Palindrome String");
+            }
+            else
+            {
+                Console.WriteLine(str + " Not Palindrome String");
+            }
+        }
+    }
+}
