0b8dac7cca22b2397e77edc65a68ed0b7abb7ca0
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-03-31 07:00:35 +0530

    Add files via upload

---
+++ ReverseString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            /* i/p: java
+             o/p: avaj*/
+
+            string str = "java";
+            Console.WriteLine("Input string: {0}", str);
+
+            char[] arr = str.ToCharArray();
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+               
+                Console.Write(arr[i]);  //Reverse string
+            }
+
+
+
+        }
+    }
+}
