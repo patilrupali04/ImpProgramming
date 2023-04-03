dbd16cc687721cc2e5a0dac2c23e23a89e586725
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-04-03 09:15:54 +0530

    Add files via upload

---
+++ ListOfPrimeNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number4
+{
+    class ListOfPrimeNum
+    {
+        static void Main(string[] args)
+        {
+            //print the prime number from 7 to 70
+            for(int i = 7; i <= 70; i++)
+            {
+                int num = i;
+                int count = 0;
+                for(int j = 1; j <= num; j++)
+                {
+                    if (num % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
