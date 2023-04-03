88426827cc109ae26aae17105a155174968ea9df
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-04-03 09:14:46 +0530

    Add files via upload

---
+++ PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number3
+{
+    class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Number is Prime {0}", num);
+            }
+            else
+            {
+                Console.WriteLine("Number is not Prime {0}", num);
+            }
+
+        }
+    }
+}
